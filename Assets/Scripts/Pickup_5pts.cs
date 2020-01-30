3a9f8862b3abf7803216dcf90d29464322184613
TabrisXing <xingtianx@gmail.com>
2020-01-30 18:34:33 -0500

    add movement to pickups

--- Assets/Scripts/Pickup_5pts.cs
+++ Assets/Scripts/Pickup_5pts.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class Pickup_5pts : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
